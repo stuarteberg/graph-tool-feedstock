329ed0329e04a856a8edd02cc817ecee99c64314
Stuart Berg <bergs@janelia.hhmi.org>
2018-04-07 20:05:16 -0400

    Fix LDFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export CPPFLAGS="-I${PREFIX}/include"
-export CXXFLAGS="-I${PREFIX}/include"
-export LDFLAGS="-Wl,-rpath,${PREFIX}/lib, -L${PREFIX}/lib"
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export BOOST_ROOT="${PREFIX}"
 
 ./configure --prefix="${PREFIX}" --with-boost="${BOOST_ROOT}" --with-boost-libdir="${BOOST_ROOT}/lib" --with-boost-python="${BOOST_ROOT}/lib/libboost_python3.dylib"

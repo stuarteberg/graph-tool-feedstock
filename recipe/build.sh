f7770afba5e1a39fb5b05ddae061dc3cd860016a
Stuart Berg <bergs@janelia.hhmi.org>
2018-04-08 10:28:11 -0400

    Downgrade boost for compatibility with the rest of my stack

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,21 +5,22 @@ export CXXFLAGS="${CXXFLAGS} -I${PREFIX}/include -std=c++14"
 export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 export BOOST_ROOT="${PREFIX}"
 
-# Requires gcc-5 on Linux, but we want to use the pre-c++11 ABI nonetheless
-#
-# How to install and enable gcc-5:
-#
-# yum install -y centos-release-scl yum-utils devtoolset-4-binutils devtoolset-4-gcc devtoolset-4-gcc-c++
-# source /opt/rh/devtoolset-4/enable
-#
-# After building this package, don't forget to switch back to gcc-4.9 if you're using that for your other packages:
-# 
-# yum install -y centos-release-scl yum-utils devtoolset-3-binutils devtoolset-3-gcc devtoolset-3-gcc-c++
-# source /opt/rh/devtoolset-3/enable
-
 if [[ $(uname) == Linux ]]; then
-    export CPPFLAGS="${CPPFLAGS} -D _GLIBCXX_USE_CXX11_ABI=0"
     export BOOST_PYTHON="${BOOST_ROOT}/lib/libboost_python3.so"
+
+	# Requires gcc-5 on Linux, but we want to use the pre-c++11 ABI nonetheless.
+	#
+	# How to install and enable gcc-5:
+	#
+	# yum install -y centos-release-scl yum-utils devtoolset-4-binutils devtoolset-4-gcc devtoolset-4-gcc-c++
+	# source /opt/rh/devtoolset-4/enable
+	#
+	# After building this package, don't forget to switch back to gcc-4.9 if you're using that for your other packages:
+	# 
+	# yum install -y centos-release-scl yum-utils devtoolset-3-binutils devtoolset-3-gcc devtoolset-3-gcc-c++
+	# source /opt/rh/devtoolset-3/enable
+    export CPPFLAGS="${CPPFLAGS} -D _GLIBCXX_USE_CXX11_ABI=0"
+
 elif [[ $(uname) == Darwin ]]; then
     export BOOST_PYTHON="${BOOST_ROOT}/lib/libboost_python3.dylib"
 fi
